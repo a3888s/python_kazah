6165c8033da0a355e620008a42005059707ee167
a3888s <sowdirect@gmail.com>
2023-02-22 15:17:20 +0200

    2

---
+++ sql_ptoject/user_insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO User(email, password, birthday)
+VALUES
+    ("sowdirect@gmail.com", "52432225", "1992-03-24"),
+    ("ramazan@gmail.com", "abcd1234", "1992-01-25"),
+    ("nikita.python@gmail.com", "thebest123", "2000-08-26"),
+    ("tilek@gmail.com", "moonsun2023", "2002-01-15");
