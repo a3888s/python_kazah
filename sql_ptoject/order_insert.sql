a69f4d91cab5b2b8da7558d0a8434afe7bcdb99f
a3888s <sowdirect@gmail.com>
2023-02-22 15:53:35 +0200

    3

---
+++ sql_ptoject/order_insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO Shop_order(user_id, status, address, mobile)
+VALUES
+    ("1", "new", "Ahunbaeva 95", "0999134494"),
+    ("2", "confirmed", "Baitik Vatyra 7A", "0777 666 444"),
+    ("4", "new", "Київська 78/2", "0555333222");
+
+--INSERT INTO Order_item(order_id, product_id, quantity)
+--VALUES
+--    ("", "", "")
